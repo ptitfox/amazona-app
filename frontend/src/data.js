6fc76a6e41c86597ad44aa52ffdca207b727bc6f
Laurent de la CLERGERIE <laurent@ldlc.com>
2021-03-31 15:53:43 +0200

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Adidas fit Shirt',
+            category:'shirts',
+            image:'/images/p2.jpg',
+            price: 50,
+            brand:'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Lacoste free Shirt',
+            category:'shirts',
+            image:'/images/p3.jpg',
+            price: 90,
+            brand:'Lacoste',
+            rating: 5,
+            numReviews: 7,
+            description: 'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Pant',
+            category:'pants',
+            image:'/images/p4.jpg',
+            price: 150,
+            brand:'Nike',
+            rating: 3,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Puma Slim Pant',
+            category:'pants',
+            image:'/images/p5.jpg',
+            price: 80,
+            brand:'Puma',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'high quality product'
+        },
+        {
+            _id:'6',
+            name:'Adidas fit Pant',
+            category:'pants',
+            image:'/images/p6.jpg',
+            price: 75,
+            brand:'Adidas',
+            rating: 4,
+            numReviews: 3,
+            description: 'high quality product'
+        }
+    ]
+};
+
+export default data;
